a8e230b1cc6a0545c24cc03a72587b58c6a8a1cc
gongjoo72 <84562833+gongjoo72@users.noreply.github.com>
2021-06-11 17:58:47 +0900

    Add files via upload

---
+++ schedule/js/jquery.index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  //Lightslider Plugin Code
+  $(".intro").lightSlider({
+    item:1,
+    pager:false,
+    slideMargin: 0,
+    loop:true,
+    speed: 400, //ms'
+    auto: true,
+    pause: 7000,
+    mode:'fade',
+    adaptiveHeight:true,
+  });
+
+  //$(".each-btns a").eq(0).trigger("click");
+  $(".each-btns a").click(function(e){
+    e.preventDefault();
+  });
+});
